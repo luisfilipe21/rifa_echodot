bb5bddd3e05e9b9877a20abe362dec3ccf35ec04
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2023-12-05 19:54:37 -0300

    Projeto final

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { Card } from "./components/Card"
+import { Header } from "./components/Header"
+import "./styles/index.scss"
+
+function App() {
+  
+  return (
+    <main className="container">
+      <Header />
+      <Card />
+    </main>
+  )
+}
+
+export default App
